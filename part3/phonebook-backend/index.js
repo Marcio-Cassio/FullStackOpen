9ea9a431bf620915a307af19942e3d55026cd20e
Marcio <marcioscontact@gmail.com>
2026-02-10 14:55:06 -0300

    Finished exercise morgan logging (3.7 - 3.8)

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+app.use(morgan('tiny'))
 app.use(express.json())
+
+morgan.token('body', (req) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body)
+    }
+    return ''
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { id: 1, name: 'Arto Hellas', number: '040-123456' },
